860422f52ca9fc68ee1322b11023a2a92d92caa5
yisusyrama <quinterosyisus@gmail.com>
2018-09-07 11:06:26 -0300

    gdfgdfg

--- flask1.py
+++ flask1.py
@@ -1,10 +1,49 @@
 from flask import *
+from DataBase import DB
+from Usuario import Usuario
+from Post import Post
+from Like import Like
+from Comment import Comment
+from UHU import UHU
+from Etiqueta import Etiqueta
+
+DB().SetConnection('127.0.0.1', 'root', 'alumno', 'mydb')
+
+unUsuario = Usuario()
+unPost = Post()
+unLike = Like()
+unUHU = UHU()
+unComment = Comment()
+unaEtiqueta = Etiqueta()
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route("/", methods=['GET', 'POST'])
+def Logeoin():
+    Usuario = request.form.get("Usuario")
+    Contra = request.form.get("Contrasena")
+    Email = request.form.get("Email")
+    Nombre = request.form.get("Nombre")
+    Bio = request.form.get("Bio")
+
+
+
+    unUsuario.Registro(Usuario, Contra, Email, Nombre, Bio)
+
     return render_template("helloworld.html")
+
+@app.route("/Registro", methods=['GET', 'POST'])
+def Register():
+    return render_template("Registro.html")
+
+@app.route("/Inicio")
+def Inicio():
+    return render_template("Inicio.html")
+
+@app.route("/Perfil")
+def Perfil():
+    return render_template("Perfil.html")
 
 
 if __name__ == '__main__':
